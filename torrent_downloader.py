0f2a2fda42af3ca50838385a3ff9192d83fc59ff
PG <smartPG@gmail.com>
2012-08-23 15:31:19 +0800

    add argv support

--- torrent_downloader.py
+++ torrent_downloader.py
@@ -2,11 +2,13 @@
 import win32utf8
 import codecs
 import http.cookiejar, urllib.request
-import re, os
+import re, os, sys
 
+
+print(len(sys.argv))
 
 cj = http.cookiejar.MozillaCookieJar()
-cj.load("cookie.txt")
+cj.load("./tmp_files/cookie.txt")
 for ind, cookie in enumerate(cj):
     print("%d - %s" %(ind, cookie))
 print("CJ END")
@@ -14,18 +16,23 @@ print("CJ END")
 
 
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-#main_page = opener.open("http://share.dmhy.org/").read()
-#open("webtmp.txt", "wb").write(main_page)
-#os.system("node node_html_tag.js")
+
+if len(sys.argv) > 1:
+	print("Reading " + sys.argv[1])
+	main_page = opener.open(sys.argv[1]).read()
+else:
+	main_page = opener.open("http://share.dmhy.org/").read()
+open("./tmp_files/webtmp.txt", "wb").write(main_page)
+os.system("node node_html_tag.js")
 filter_data = codecs.open("filter.txt", "r", "utf8").readlines()
 filter_list = []
 for i in filter_data:
 	#print(i)
-	tmp = i.split(" ")
+	tmp = i.strip().split(" ")
 	#print(tmp)
 	filter_list.append(tmp)
-
-main_page = codecs.open("parsed_web.txt", "r", "utf8").readlines()
+#print(filter_list)
+main_page = codecs.open("./tmp_files/parsed_web.txt", "r", "utf8").readlines()
 torrent_list = []
 while len(main_page):
 	i = main_page.pop(0)
@@ -33,8 +40,11 @@ while len(main_page):
 	torrent_list.append((i,j))
 #print(torrent_list)
 download_list = []
+skip_count = 0
+ignore_count = 0
 for title,torrent in torrent_list:
 	#print(title, torrent)
+	add_flag = False
 	for rule in filter_list:
 		#print("testing rule:", rule)
 		flag = True
@@ -48,16 +58,24 @@ for title,torrent in torrent_list:
 		if flag:
 			#print(title, "passed", rule)
 			download_list.append((title, torrent, rule))
+			add_flag = True
+			break
+	if not add_flag:
+		ignore_count += 1
 for title,torrent,rule in download_list:
-	print(title, torrent, rule) 
+	#print(title, torrent, rule) 
+	print(title, rule)
 	name = "./torrents/" + re.search("hash_id\/([A-Za-z0-9]+)", torrent).group(1) + ".torrent"
 	if os.path.exists(name):
 		print("File already exists!")
+		skip_count += 1
 	else:
 		torrent_data = opener.open("http://share.dmhy.org" + torrent).read()
 		open(name, "wb").write(torrent_data)
 		print("Downloaded 1 torrent file.")
 
+print("Skip " + str(skip_count) + " file(s).")
+print("Ignore " + str(ignore_count) + " file(s).")
 
 
 
