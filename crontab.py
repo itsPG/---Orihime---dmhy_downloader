0f10be7e9b377472e246d3782e7616b7a4259544
PG <smartPG@gmail.com>
2012-10-24 04:24:14 +0800

    crontab

--- crontab.py
+++ crontab.py
@@ -1,6 +1,6 @@
 import time, os, random
 while True:
-	os.system("python torrent_downloader.py")
-	r = random.randint(600,1200);
+	os.system("python torrent_downloader.py http://share.dmhy.org/topics/list/sort_id/2")
+	r = random.randint(900,1800);
 	print("Sleep for " + str(r) + " seconds....................")
 	time.sleep(r)
